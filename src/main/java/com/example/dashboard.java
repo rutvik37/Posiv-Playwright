deecc1e21507b6cc630a894064399e65d6070f7d
Neel Prajapati <neelprajapati@hyperlinkinfosystem.net.in>
2025-04-09 18:57:16 +0530

    new changes

---
+++ src/main/java/com/example/dashboard.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+
+public class dashboard {
+
+    public void dashboard(Page page) {
+        try {
+
+            
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Dashboard")).click();
+            page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Total Customers")).click();
+            Thread.sleep(1000);
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Dashboard")).click();
+
+            page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Total Advertisment")).click();
+            Thread.sleep(1000);
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Dashboard")).click();
+
+            page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Total Post")).click();
+            Thread.sleep(1000);
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Dashboard")).click();
+
+            page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Total Earnings")).click();
+            Thread.sleep(1000);
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Dashboard")).click();
+
+            page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Total Contact Request")).click();
+            Thread.sleep(1000);
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Dashboard")).click();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/posiv.java
+++ src/main/java/com/example/posiv.java
@@ -4,91 +4,33 @@ import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
 
 public class posiv {
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-                    .setHeadless(false));
+
+            
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             BrowserContext context = browser.newContext();
             Page page = context.newPage();
             page.navigate("https://admin.posiv.org.uk/#");
-
+            
             page.getByPlaceholder("Enter Email").fill("admin@posiv.com");
             page.getByPlaceholder("Enter Password").fill("Admin@111");
-            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Log in")).click(); 
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Log in")).click();
 
+            Thread.sleep(2000); // Wait for login
 
+            // Dashboard
+            dashboard dash = new dashboard();
+            dash.dashboard(page);
 
-            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Dashboard")).click();
-      page.locator("#headlessui-menu-button-1").click();
-      page.getByRole(AriaRole.MENUITEM, new Page.GetByRoleOptions().setName("Edit Profile")).click();
-      page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Save changes")).click();
-      page.locator("#headlessui-menu-button-1").click();
-      page.getByLabel("").click();
-      page.getByLabel("Close").click();
+            // Edit Profile
+            edit_profile profile = new edit_profile();
+            profile.edit_profile(page);
 
-
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Customers").setExact(true)).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Reported Customers")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Advertisement")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Posts")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Reported Content")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Report Reasons")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Document Verification")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Directory Services")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Story Stickers")).click();
-     
-      
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Leader Board")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Joining Waitlist")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Survey Records")).click();
-    
-      page.getByText("DashboardCustomersReported").click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Survey Records")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Earnings")).click();
-      
-     
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Manage Subadmin")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Send Notification")).click();
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Contact Us")).click();
-      
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("FAQ")).click();
-      page.getByText("DashboardCustomersReported").click();
-      
-      page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("CMS")).click();
-      
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("About Us")).click();
-      //page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("About Us")).press("ArrowDown");
-      
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Privacy Policy")).click();
-   
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Terms & Condition")).click();
-      
-      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Why Posiv")).click();
-
-      page.locator("#headlessui-menu-button-1").click();
-      page.getByRole(AriaRole.MENUITEM, new Page.GetByRoleOptions().setName("Log Out")).click();
-      page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Yes, Log Me Out!")).click();
-
-            
-
-        
-            // try {d
-            //     Thread.sleep(5000);
-            // } catch (Exception e) {
-
-            // }
-
-            playwright.close();
-            browser.close();
-
-            try {
-                
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
 }
